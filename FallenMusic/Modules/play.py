fb2604876278a74560d87eb5fd2d5a6ecdb5e28a
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-03-25 00:40:02 +0530

    Update play.py
    Signed-off-by: Raj pomane  <95400073+Rajpomane@users.noreply.github.com>

--- FallenMusic/Modules/play.py
+++ FallenMusic/Modules/play.py
@@ -179,7 +179,7 @@ async def play(_, message: Message):
     else:
         if len(message.command) < 2:
             return await fallen.edit_text("» ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴘʟᴀʏ ʙᴀʙʏ ?")
-        await fallen.edit_text("🔎")
+        await fallen.edit_text("🚩🚩🚩")
         query = message.text.split(None, 1)[1]
         try:
             results = YoutubeSearch(query, max_results=1).to_dict()
