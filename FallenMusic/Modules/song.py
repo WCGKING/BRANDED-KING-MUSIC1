6534a253876ca5eb187df967c0f4e790f45a15fe
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-03-27 00:35:34 +0530

    Update song.py
    Signed-off-by: Raj pomane  <95400073+Rajpomane@users.noreply.github.com>

--- FallenMusic/Modules/song.py
+++ FallenMusic/Modules/song.py
@@ -64,7 +64,7 @@ async def song(_, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☁️ **ᴛɪᴛʟᴇ :** [{title[:23]}]({link})\n⏱️ **ᴅᴜʀᴀᴛɪᴏɴ :** `{duration}`\n🥀 **ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ :** {BOT_MENTION}"
+        rep = f"🚩 **ᴛɪᴛʟᴇ 🚩 :** [{title[:23]}]({link})\n🚩 **ᴅᴜʀᴀᴛɪᴏɴ🚩:** `{duration}`\n🥀 🚩**ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ🚩 :** {BOT_MENTION}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
