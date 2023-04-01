312909b5954fe7f03e6317aa0a1add81d7870572
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-04-02 02:33:31 +0530

    Update config.py
    Signed-off-by:  BRANDED KING  <121561060+WCGKING@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,14 +13,14 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/c97c1a3bea4f3a5762bca.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/c97c1a3bea4f3a5762bca.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/ebcf5d6354a44ff833cc7.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/ebf34649780915d5951fe.jpg")
 
 SESSION = getenv("SESSION", None)
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/LOVERS_POINTT")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/LOVERS_POINTT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDED_KHUSHI")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 
-FAILED = "https://te.legra.ph/file/c97c1a3bea4f3a5762bca.jpg"
+FAILED = "https://te.legra.ph/file/6a4ac076ff159bdcf9656.jpg"
