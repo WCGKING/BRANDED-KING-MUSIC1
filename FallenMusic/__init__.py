9ca6c4dc4fa9a49a049685483d65e0b911f8b565
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-27 14:40:07 +0530

    BRANDED KING MUSIC 1

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("FallenAssociation")
+        await app2.join_chat("BRANDED_WORLD")
+        await app2.join_chat("BRANDRD_BOT")
     except:
         pass
 
