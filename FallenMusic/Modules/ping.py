c92e3721f4e589c304b6e0fa256d6eec9a8c0665
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-27 19:32:36 +0530

    Update ping.py

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -61,8 +61,5 @@ async def ping_fallen(_, message: Message):
                     InlineKeyboardButton("✭ 𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𝗦𝗨𝗣𝗣𝗢𝗧 🥀", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton("✭ 𝗕𝗥𝗡𝗗𝗥𝗗 𝗕𝗢𝗧 🥀",url="https://t.me/BRANDRD_BOT",
                     InlineKeyboardButton("✭ 𝐁𝐑𝐀𝐍𝐃𝐄𝐃 𝐏𝐀𝐈𝐃 𝐇𝐄𝐑𝐎𝐊𝐔 𝐂𝐂 🥀",url="https://t.me/BRANDED_PAID_CC",
-                    ),
-                ]
-            ]
-        ),
-    )
+                [   
+                  
