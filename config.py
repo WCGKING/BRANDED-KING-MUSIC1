1358c85a13c385e939587a6dbb1b91bde9fae82c
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-09-30 12:29:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,4 +23,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BRANDRD_BOT")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "KHUSHI_MUSIC76_BOT")  
-YOUTUBE_IMG_URL:"assets/Youtube.jpeg":
+YOUTUBE_IMG_URL = "assets/Youtube.jpeg"
